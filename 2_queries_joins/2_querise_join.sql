47d1e83996f2fff7a4707e2cb197398cfb8f5a96
Frank Shen <frank851207@gmail.com>
2023-06-27 16:28:06 -0700

    query3 GROUP BY init

--- 2_queries_joins/2_querise_join.sql
+++ 2_queries_joins/2_querise_join.sql
@@ -9,3 +9,46 @@ JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
 WHERE cohorts.name = 'FEB12';
 
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+HAVING count(assignment_submissions.*) <100;
+
+SELECT day, count(*) as total_assignments 
+FROM assignments
+WHERE total_assignments > 10
+GROUP BY day
+ORDER BY day;
+
+SELECT count(students.*) as student_count, cohorts.name as cohort_name
+FROM cohorts
+JOIN students on cohorts.id = cohort_id
+GROUP BY cohort_name
+HAVING count(students.*) >= 18
+ORDER BY student_count;
+
+SELECT cohorts.name as cohort, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY total_submissions DESC;
+
+
+SELECT students.name as student, AVG(assignment_submissions.duration) as average_assignment_duration
+FROM assignment_submissions
+JOIN students ON students.id = assignment_submissions.student_id
+WHERE end_date IS NULL
+GROUP BY student
+ORDER BY average_assignment_duration DESC;
+
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration, avg(assignments.duration) as average_estimated_duration
+FROM students
+JOIN assignment_submissions ON student_id = students.id
+JOIN assignments ON assignments.id = assignment_id
+WHERE end_date IS NULL
+GROUP BY student
+HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+ORDER BY average_assignment_duration;
