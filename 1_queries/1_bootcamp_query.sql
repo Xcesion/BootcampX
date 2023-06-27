af3e115f634a8343a45ba771bc2f289e9fab8fa0
Frank Shen <frank851207@gmail.com>
2023-06-27 00:35:24 -0700

    Non Gmail Students get all the students without a gmail.com and a phone number

--- 1_queries/1_bootcamp_query.sql
+++ 1_queries/1_bootcamp_query.sql
@@ -15,5 +15,10 @@ WHERE cohort_id IN (1,2,3);
 
 SELECT name, id, cohort_id
 FROM students
-WHERE email IS NULL
-OR phone IS NULL;
+WHERE phone IS NULL
+OR  IS NULL;
+
+SELECT name, id, email, cohort_id
+FROM students
+WHERE email NOT LIKE '%gmail.com'
+AND phone IS NULL;
