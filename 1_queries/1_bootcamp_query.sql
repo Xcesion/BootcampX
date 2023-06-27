f124b88f2f54d5602f6213d1c7db3c101a9adea3
Frank Shen <frank851207@gmail.com>
2023-06-27 14:51:34 -0700

    added query join files

--- 1_queries/1_bootcamp_query.sql
+++ 1_queries/1_bootcamp_query.sql
@@ -35,3 +35,18 @@ WHERE github IS NULL
 AND end_date IS NOT NULL;
 
 
+
+
+SELECT students.name as student_name, cohorts.name as cohort_name,
+cohorts.start_date as cohort_start_date,students.start_date as student_start_date
+FROM students JOIN cohorts ON cohort_id =cohorts.id
+WHERE cohorts.start_date != students.start_date
+ORDER BY cohort_start_date;
+
+SELECT sum(assignment_submissions.duration) as total_duration
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.name = 'Ibrahim Schimmel';
+
+SELECT cohorts.name, cohorts.duration FROM chohorts RIGHT OURTER 
+JOIN assignement_submissions ON cohorts.durations = assignment_submissions.duration WHERE cohorts.name = 'FEB12';
